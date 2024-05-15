2db43ba6047cf9ed2b48b8d7a547553e96b3b101
AgitoSun <zeke1@mail.ru>
2024-05-15 12:15:35 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
                 'resources/assets/js/scripts.js',
                 'resources/assets/js/demo-settings.js',
                 'resources/assets/js/apps/file-manager.js',
+                'resources/assets/js/apps/file-modal.js',
             ],
             refresh: true,
         }),
