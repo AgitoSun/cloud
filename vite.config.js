a18b7621e5b63baeea437a1803390e56461b17a0
AgitoSun <zeke1@mail.ru>
2024-05-15 10:27:07 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/assets/img/favicon/favicon.png',
+                'resources/assets/css/dashlite.css',
+                'resources/assets/css/theme.css',
             ],
             refresh: true,
         }),
