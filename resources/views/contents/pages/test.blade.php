d955d1e9b504b17ecae7e56404949d0b0005e86e
AgitoSun <zeke1@mail.ru>
2024-05-16 14:28:02 +0300

    first commit

--- resources/views/contents/pages/test.blade.php
+++ resources/views/contents/pages/test.blade.php
@@ -3189,7 +3189,7 @@
     <div class="pmo-st-text">Looking for Advanced <br> Investment Platform?</div>
 </a>
 @vite('resources/js/app.js')
-<script src="js/bundle.js"></script>
-<script src="js/scripts.js"></script>
+{{--<script src="js/bundle.js"></script>--}}
+{{--<script src="js/scripts.js"></script>--}}
 </body>
 </html>
