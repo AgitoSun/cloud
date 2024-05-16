387b463be0ff6ee7a27bcfbbfea269180ed0e690
AgitoSun <zeke1@mail.ru>
2024-05-16 12:58:33 +0300

    first commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import '../assets/js/bundle';
 import '../assets/js/scripts';
 import '../assets/js/demo-settings';
 import '../assets/js/apps/file-manager';
-import '../assets/js/apps/file-modal';
+// import '../assets/js/apps/file-modal';
 
 import Alpine from 'alpinejs';
 
