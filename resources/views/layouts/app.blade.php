f35caa292095514e4664dd7c44d155c12fcddd9f
AgitoSun <zeke1@mail.ru>
2024-05-16 13:03:48 +0300

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
 @include('layouts/modal/file-share')
 @include('layouts/modal/file-details')
 @include('layouts/modal/file-upload')
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 @vite('resources/js/app.js')
 @include('layouts/scripts')
 @yield('script')
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,7 +5,6 @@
     <link rel="shortcut icon" href="{{ Vite::asset('resources/assets/img/favicon/favicon.png') }}">
     <title>@yield('title') | ДИАС - Файловый менеджер</title>
     <link rel="stylesheet" href="{{ Vite::asset('resources/assets/css/dashlite.css') }}">
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-91615293-4"></script>
 {{--    @vite('resources/js/app.js')--}}
 </head>
