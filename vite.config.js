11f23dfeeab6ed66c7bd0d6e50c1984bc5241f92
AgitoSun <zeke1@mail.ru>
2024-05-16 16:13:01 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import jQuery from 'jquery';
-window.$ = jQuery;
-window.jQuery = jQuery;
+// import jQuery from 'jquery';
+// window.$ = jQuery;
+// window.jQuery = jQuery;
 
 export default defineConfig({
     plugins: [
