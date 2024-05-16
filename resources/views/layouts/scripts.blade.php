10b78039dbe872b58a1462396a322300e205c4fe
AgitoSun <zeke1@mail.ru>
2024-05-16 16:14:24 +0300

    first commit

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -2,3 +2,5 @@
 {{--<script src="{{ Vite::asset('resources/assets/js/scripts.js') }}"></script>--}}
 {{--<script src="{{ Vite::asset('resources/assets/js/demo-settings.js') }}"></script>--}}
 {{--<script src="{{ Vite::asset('resources/assets/js/apps/file-manager.js') }}"></script>--}}
+<script src="{{ asset('js/bundle.js') }}"></script>
+<script src="{{ asset('js/scripts.js') }}"></script>
