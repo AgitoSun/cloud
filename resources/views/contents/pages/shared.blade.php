f12656210a8388e272713c7386239cfdf4f05929
AgitoSun <zeke1@mail.ru>
2024-05-27 09:35:06 +0300

    fix error

--- resources/views/contents/pages/shared.blade.php
+++ resources/views/contents/pages/shared.blade.php
@@ -159,14 +159,24 @@
 
         const inputEl = document.querySelector('.to-copy');
 
-        readBtn.addEventListener('click', () => {
-            navigator.clipboard.readText()
-                .then(text => {
-                    resultsEl.innerText = text;
-                })
-                .catch(err => {
-                    console.log('Something went wrong', err);
-                })
+        writeBtn.addEventListener('click', () => {
+            const inputValue = inputEl.value.trim();
+            if (inputValue) {
+                navigator.clipboard.writeText(inputValue)
+                    .then(() => {
+                        inputEl.value = '';
+                        if (writeBtn.innerText !== 'Copied!') {
+                            const originalText = writeBtn.innerText;
+                            writeBtn.innerText = 'Copied!';
+                            setTimeout(() => {
+                                writeBtn.innerText = originalText;
+                            }, 1500);
+                        }
+                    })
+                    .catch(err => {
+                        console.log('Something went wrong', err);
+                    })
+            }
         });
     </script>
 @endsection
